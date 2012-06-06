f95633c7ac2ea9f65d08ba78c03f5c326a990263
leo <wlong@yintai.com>
2012-06-06 14:27:57 +0800

    yintai tools
    item promotions activity infomation robot

---
+++ YinTai.Tools.ItemPromotionsActivityInfo.Contract/IProductPromotionsActivityInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace YinTai.Tools.ItemPromotionsActivityInfo.Contract
+{
+    [ServiceContract]
+    public interface IProductPromotionsActivityInfo
+    {
+
+    }
+}
---
+++ YinTai.Tools.ItemPromotionsActivityInfoTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("YinTai.Tools.ItemPromotionsActivityInfoTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("YinTai.Tools.ItemPromotionsActivityInfoTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6ce845fb-9c0c-4588-a2dc-f1544393ee61")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
